97038033f29127bb76e3db913d70932d71d9d1a2
Teresh <tereshvti@gmail.com>
2013-08-13 17:02:01 +0300

    smth done

--- index.php
+++ index.php
@@ -12,5 +12,41 @@ class Modes
   else
     echo 'Resize failed!';
   echo '<br>';
+  
+  $test = 'test2';
+  $dir = getcwd() . "\\images\\cache\\" . $test;
+  if (!file_exists($dir)) {
+    echo '<br> директории нет, надо бы создать';
+	mkdir($dir);
+  }
+  
+  
+  function listFolderFiles($dir){
+    $ffs = scandir($dir);
+    echo '<ol>';
+    foreach($ffs as $ff){
+        if($ff != '.' && $ff != '..' && is_dir($ff)){
+            echo '<li>'.$ff;
+            if(is_dir($dir.'/'.$ff)) listFolderFiles($dir.'/'.$ff);
+            echo '</li>';
+        }
+    }
+    echo '</ol>';
+  }
+  function is_empty_dir($dir){
+    echo $dir . '<br>';
+    $ffs = scandir($dir);
+	$result = array();
+    foreach($ffs as $ff){
+        if($ff != '.' && $ff != '..' && is_dir($ff)){
+			array_push($result, $ff);
+			echo '<br>' . $ff;
+        }
+    }
+	echo var_dump($result) . '<br>';
+	return $result;
+  }
+  is_empty_dir(getcwd() . "\\images\\cache\\");
+  //echo var_dump(is_empty_dir($dir));
   //echo var_dump(getimagesize('original.jpg'));
 ?>
